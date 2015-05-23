ee2e828354160f80d653086c80dcb730c391c622
Admin <Admin@D20927>
2015-05-23 14:35:06 +0700

    05

---
+++ example05/src/example05/Polygon.java
@@ -0,0 +1,13 @@
+package example05;
+
+public class Polygon {
+	protected int width,height;
+	public Polygon (int a){
+		width=a;
+	}
+	public void setValuas(int a,int b)
+	{
+		width=a;
+		height=b;
+		}
+}
---
+++ example05/src/example05/main.java
@@ -0,0 +1,10 @@
+package example05;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle rect = new Rectangle(8,9);
+System.out.println("rect a:{0} "+ rect.area());
+	}
+}
